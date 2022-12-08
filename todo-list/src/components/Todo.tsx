3926a306789f6789c75ecc343857ea18e9dee364
sc30gsw <haidoushi875@gmail.com>
2022-12-08 13:40:53 +0900

    todoListの初期オブジェクトの定義

--- todo-list/src/components/Todo.tsx
+++ todo-list/src/components/Todo.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { TodoList } from "../types/TodoList";
 
 const Todo = () => {
+	const todosList: TodoList[] = [
+		{
+			id: 1,
+			content: "店予約する",
+		},
+		{
+			id: 2,
+			content: "卵買う",
+		},
+		{
+			id: 3,
+			content: "郵便出す",
+		},
+	];
+
 	return <div></div>;
 };
 
