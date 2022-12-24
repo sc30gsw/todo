e1f83ccd99b55ea922eaa0c78c4bee9e4e8548bb
sc30gsw <haidoushi875@gmail.com>
2022-12-24 11:55:28 +0900

    JSDocの追加

--- todo-list/src/components/Item.tsx
+++ todo-list/src/components/Item.tsx
@@ -18,14 +18,31 @@ type Props = {
 const Item = ({ todo, deleteTodo, updateTodo }: Props) => {
 	const [editContent, setEditContent] = useState<string>(todo.content);
 
+	/**
+	 * 入力値をuseStateに設定する関数
+	 *
+	 * @param e ChangeEvent
+	 * @returns  void
+	 */
 	const changeEditContent = (e: React.ChangeEvent<HTMLInputElement>) =>
 		setEditContent(e.target.value);
 
+	/**
+	 * テキスト(span)と入力欄(input)の切り替えを行う関数
+	 *
+	 * @returns void
+	 */
 	const toggleEditMode = () => {
 		const newTodo = { ...todo, editing: !todo.editing };
 		updateTodo(newTodo);
 	};
 
+	/**
+	 * Todoの更新を送信するための関数
+	 *
+	 * @param e FormEvent
+	 * @returns void
+	 */
 	const confirmEditTodo = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const newTodo = { ...todo, content: editContent, editing: !todo.editing };
