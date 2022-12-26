f2fdb54ae278f8277baba5deb54c1bf15c689562
sc30gsw <haidoushi875@gmail.com>
2022-12-26 21:26:59 +0900

    グローバルコンテキストに書き換え、不要な記述を削除

--- todo-list/src/components/Todo.tsx
+++ todo-list/src/components/Todo.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { TodoList } from "../types/TodoList";
+import TodoProvider from "../context/TodoContext";
 import Form from "./Form";
 import List from "./List";
 
@@ -9,63 +8,11 @@ import List from "./List";
  * @returns
  */
 const Todo = () => {
-	const todosList: TodoList[] = [
-		{
-			id: 1,
-			content: "店予約する",
-			editing: false,
-		},
-		{
-			id: 2,
-			content: "卵買う",
-			editing: false,
-		},
-		{
-			id: 3,
-			content: "郵便出す",
-			editing: false,
-		},
-	];
-
-	const [todos, setTodos] = useState<TodoList[]>(todosList);
-
-	/**
-	 * Todoの削除を行う関数
-	 *
-	 * @param id number
-	 * @returns void
-	 */
-	const deleteTodo = (id: number) => {
-		const newTodos: TodoList[] = todos.filter((todo) => todo.id !== id);
-		setTodos(newTodos);
-	};
-
-	/**
-	 * Todoの登録を行う関数
-	 *
-	 * @param todo {TodoList}
-	 * @returns void
-	 */
-	const createTodo = (todo: TodoList) => setTodos([...todos, todo]);
-
-	/**
-	 * Todoの更新を行う関数
-	 *
-	 * @param todo {TodoList}
-	 * @returns void
-	 */
-	const updateTodo = (todo: TodoList) => {
-		const newTodos = todos.map((_todo) => {
-			return _todo.id === todo.id ? { ..._todo, ...todo } : { ..._todo };
-		});
-		setTodos(newTodos);
-	};
-
 	return (
-		<>
-			<List todos={todos} deleteTodo={deleteTodo} updateTodo={updateTodo} />
-			<Form createTodo={createTodo} />
-		</>
+		<TodoProvider>
+			<List />
+			<Form />
+		</TodoProvider>
 	);
 };
 
